84ad782d22a9925abda24ce2b83d9c48dd5e683e
RodrigoBarreiro <barreiro.rb@gmail.com>
2022-10-22 13:53:52 -0300

    ultimos pasos de proyecto

---
+++ js/carrito.js
@@ -0,0 +1,86 @@
+class Carrito{
+    constructor(cervezasEnStock){
+        this.cervezasEnStock = cervezasEnStock;
+        /*
+        * Si tengo un carrito ya guardado previamente lo asigno a this.carrito sino inicializo uno nuevo
+        */
+        if(this.obtenerCarrito()){
+            this.carrito =  this.obtenerCarrito();
+        }else {
+            this.carrito = this.inicializar();
+        } 
+        console.log("[Carrito] Carrito inicializado con ", JSON.stringify(this.carrito));
+    }
+
+    /*
+    * El resultado de esta funcion es:
+    * carrito = [
+    *    1 = [new Producto(), new Producto()],
+    *    2 = [],
+    *    3 = [new Producto(), new Producto(), new Producto()]
+    *  ];
+    * Esto es dado que el carrito precisa estar ordenado por el id de la cerveza para ir contabilizando por tipo de cerveza
+    */
+    inicializar(){
+        
+        this.cervezasEnStock.forEach (cerveza => { 
+            this.carrito[cerveza.id] = [];
+        });
+    }
+
+    /*
+    * Dado un id de una cerveza, lo busca en el stock de cerveza, agrega al carrito y lo guarda
+    */
+    agregarACarrito(cervezaId){
+        console.log("[Carrito] Agregando el id ", cervezaId, "en el carrito", JSON.stringify(this.carrito));
+        const cervezaComprada = this.cervezasEnStock.find ( cerveza => {
+            return cerveza.id === parseFloat(cervezaId);
+        });
+        const carritoGuardado = this.carrito;
+        const carritoProdructoMismoId = carritoGuardado [cervezaId] ;
+        carritoProdructoMismoId.push(cervezaComprada);
+        this.guardarCarrito();
+    }
+
+    /*
+    * Obtiene la catidad de todos los productos que se encuentran en el carrito
+    */
+    obtenerCervezasCompradas(){
+        let cervezasCompradas = 0;
+        for( let cervezasPorId of this.carrito) {
+            if(Array.isArray(cervezasPorId)){
+                cervezasCompradas = cervezasCompradas + cervezasPorId.length;
+            }
+        }    
+        return cervezasCompradas;
+    }
+
+    /*
+    * Obtiene la el monto total de todos los productos que se encuentran en el carrito
+    */
+    obtenerMontoTotalCervezasCompradas(){
+        let montoTotalCervezas = 0;
+        for( let cervezasPorId of this.carrito) {
+            if(Array.isArray(cervezasPorId)){
+                for( let cerveza of cervezasPorId) {
+                    montoTotalCervezas = montoTotalCervezas + cerveza.precio;
+                }
+            }
+        }    
+        return montoTotalCervezas;
+    }
+
+    /*
+    * Obtiene el carrito del localstorage
+    */
+    obtenerCarrito(){
+        return JSON.parse(localStorage.getItem ("carrito"));
+    }
+
+    /*
+    * Guarda el carrito actual en el local storage
+    */
+    guardarCarrito(){
+        localStorage.setItem ("carrito", JSON.stringify(this.carrito));
+    }
+}
--- js/index.js
+++ js/index.js
@@ -1,12 +1,11 @@
 let fetchResult = null;
-// SE UTILIA COMANDOS npm install -g json-server Y json-server --watch ./js/data.json PARA LEVANTAR SERVIDOR DE JSON
+// SE UTILIA COMANDOS npm install && npm run start-server
 fetch("http://localhost:3000/productos")
     .then((response) => response.json())
     .then((data) => {
-    fetchResult = data;
-    init();
+        fetchResult = data;
+        init();
     });
-
 
 //SE AGREGA SWEET ALERT CON SOLICITUD DE EDAD PARA INGRESAR EN LA PAGINA PARA VENTA DE ALCOHOL
 (async function pedirEdad (){
@@ -55,12 +54,12 @@ fetch("http://localhost:3000/productos")
 })()
 
 let cervezas = [];
-let carrito = [];
+let carrito = null;
 let cervezasCompradas = 0;
 
 function init (){
     cervezas = fetchResult.map (cerveza => new Producto ( cerveza.id, cerveza.nombre, cerveza.precio, cerveza.envase, cerveza.marca, cerveza.stock, cerveza.descpricion ));
-    console.log (cervezas);
+    carrito = new Carrito(cervezas);
     //SE INCORPORA DOM CON INFORMACION NECESARIA PARA PAGINA WEB SOBRE LOS PRODUCTOS y SE INTERACTUA CON HTML.
     cervezas.forEach (cerveza => { 
         document.getElementById ("cervaza-titulo-" + cerveza.id).innerText = cerveza.nombre;
@@ -68,64 +67,59 @@ function init (){
         document.getElementById ("precio-" + cerveza.id ).innerText = " Precio: "+ cerveza.precio;
     })
     
-    // SE INICIALIZA CARRITO.  
-    inicializarCarrito ();
-
-    localStorage.setItem  ("carrito", localStorage.getItem ("carrito") || JSON.stringify(carrito));
     dibujarCarrito(); 
     
     // EVENTO A COMPRAR UN PRODUCTO.
-    document.getElementById ("cervezasCompradas").innerText= cervezasCompradas;
+    document.getElementById ("cervezasCompradas").innerText= carrito.obtenerCervezasCompradas();
     const botones = document.querySelectorAll (".comprarBtn");
     botones.forEach (boton => {
         boton.addEventListener ( "click", (event) => {
             cervezasCompradas = cervezasCompradas + 1;
             const id = event.currentTarget.dataset.id;
-            const productoComprado = cervezas.find ( cerveza => {
-                return cerveza.id === parseFloat(id);
-            })
-            const carritoGuardado = JSON.parse(localStorage.getItem ("carrito"));
-            const carritoProdructoMismoId = carritoGuardado [id] ;
-            carritoProdructoMismoId.push(productoComprado);
-            localStorage.setItem ("carrito", JSON.stringify(carritoGuardado));
+            carrito.agregarACarrito(id);
             dibujarCarrito();
         })
     })
 }
 
 function dibujarCarrito () {
-    document.getElementById ("cervezasCompradas").innerText= cervezasCompradas;
+    document.getElementById ("cervezasCompradas").innerText= carrito.obtenerCervezasCompradas();;
     const menu = document.getElementById ("carrito");
     let liHtml = "";
-    const carritoGuardado = JSON.parse(localStorage.getItem ("carrito"));
-    console.log(carritoGuardado);
+    const carritoGuardado = carrito.obtenerCarrito();
     carritoGuardado.forEach ((carritoProducto, id) => { 
         if (carritoProducto && carritoProducto.length > 0) {
-            liHtml = liHtml + "<li> <img src=\"./imagenes/"  + id + ".png\" width =\"50px\" ></img>" + carritoProducto.length + " X " + carritoProducto [0].nombre + " " + carritoProducto[0].precio + "</li>";
+            liHtml = liHtml + "<li> <img src=\"./imagenes/"  + id + ".png\" width =\"50px\" ></img>" + carritoProducto.length + " X " + carritoProducto [0].nombre + " $" + carritoProducto[0].precio + "</li>";
         }
-    }) 
+    })
+    liHtml = liHtml + "<li> Total: $"+ carrito.obtenerMontoTotalCervezasCompradas() +" </li>";
     menu.innerHTML = liHtml;
 }
 
-function inicializarCarrito (){
-    cervezas.forEach (cerveza => { 
-        carrito[cerveza.id] = [];
-    })
-    cervezasCompradas = 0;
-}
-
-// BOTON DE CANCELAR COMPRA
 document.getElementById ("borrarCarrito").addEventListener ( "click", (event) => {
-    inicializarCarrito ();
-    localStorage.setItem ("carrito", JSON.stringify(carrito));
+    carrito.inicializar();
+    carrito.guardarCarrito();
     dibujarCarrito();
 })
 
+document.getElementById ("confirmarCarrito").addEventListener ( "click", async (event) => {
+    const {value: edad } = await Swal.fire ({
+        title: 'Carrito comprado con éxito',
+        backdrop: true,
+        confirmButtonText: 'Aceptar',
+        confirmButtonColor: '#DEB887',
+        footer: `<span> <b> Usted compró ${carrito.obtenerCervezasCompradas()} cervezas por $${carrito.obtenerMontoTotalCervezasCompradas()}  </b> </span>`,
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        allowEnterKey: false
+    });
+    carrito.inicializar();
+    carrito.guardarCarrito();
+    dibujarCarrito();
+});
 
 
 
-// BOTON COMPRAR PARA FINALIZAR COMPRA 
-// SUMA TOTAL DE LA COMPRA 
 
 
 
